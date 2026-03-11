27dbd4dcbf68ce2c0e30acfa4b30edb65a16a7ed
Anuja khatri <khatrianuja20@gmail.com>
2026-03-11 21:27:50 +0545

    added tax-status

--- day2.py
+++ day2.py
@@ -1,11 +1,20 @@
-# employee_list = []
-# tax_list = []
-
+#************ Task-3-Anuja *************
 for i in range(1, 6):
+# ************* Task-1-Aishwarya ***************    
     print("\nEntering details for Employee:", i)
     name = input("Enter employee name: ")
     normal_salary = int(input("Enter your monthly salary: "))
-    tax_salary = int(input("Enter your exact salary: "))
+    salary = int(input("Enter your exact salary: "))
+# ************* Task-2-Anuska ***************
+    if salary <30000:
+        tax = 0
+    elif  salary<=6000:
+        tax = 0.1*salary
+    else:
+        tax = 0.2*salary
+
+# ************* Task-4-Asmita ***************    
+        
     
-    tax_percent = ((normal_salary - tax_salary) / normal_salary) * 100
+    
 
