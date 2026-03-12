d669ef9cbf1631dc9d3419ec257a2ee85f5038cd
Anuja khatri <khatrianuja20@gmail.com>
2026-03-12 21:43:12 +0545

    Add tax_calculation function

--- home.py
+++ home.py
@@ -1,21 +1,25 @@
 
-# *************day-2***************
+# *************day-3***************
+def calculate_tax(salary):
+    if salary < 30000:
+        tax = 0
+    elif salary <= 60000:
+        tax = 0.1 * salary
+    else:
+        tax = 0.2 * salary
+    return tax
+  
+def calculate_net_salary(salary, tax):
+    net_salary = salary - tax
+    return net_salary
 
-salary = int(input("Enter your salary: "))
-if salary <30000:
-  tax = 0
-elif  salary<=6000:
-  tax = 0.1*salary
-else:
-  tax = 0.2*salary
-net_salary = salary - tax
-print("Salary: ",salary)
-print("Tax: ",tax)
-print("Net Salary: ",net_salary)
 
-##day 3##
-
-def save_tax_record(name, salary, tax):
+def save_tax_record():
+    name= input("Enter your name:")
+    salary = int(input("Enter your salary:"))
+    tax = calculate_tax(salary)
+    net_salary=calculate_net_salary(salary,tax)
     file = open("tax_records.txt", "a")   # "a" means append (add new data)
     file.write(f"Name: {name}, Salary: {salary}, Tax: {tax}\n")
     file.close()
+    
